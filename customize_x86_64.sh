be7c314ee1a7e83ce4efa49eea35a18569f5debb
cenchale <cenchale@gmail.com>
2020-04-02 15:05:13 +0800

    config to mine

---
+++ customize_x86_64.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# modifier: cenchale
+#=================================================
+#1. Modify default IP
+#sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
